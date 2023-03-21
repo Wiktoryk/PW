9ea13aca1f7c4c1c7f3a1bd5e6a2668792088f2e
Wiktor Oliwa 242486 <242486@edu.p.lodz.pl>
2023-03-21 22:19:34 +0100

    kompilowalny program

---
+++ PW/src/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("Podaj 2 liczby,które chcesz dodać");
+        int x =Int32.Parse(Console.ReadLine());
+        int y =Int32.Parse(Console.ReadLine());
+        System.Console.WriteLine(add(x, y));
+    }
+    static int add(int x, int y) { return x + y; }
+}
